e28c9cea15635f0790a461e685df40dc047f0c85
Alex <alexis.rj2110@gmail.com>
2025-07-04 18:18:21 -0500

    V0.0.6

--- ejemplo7/proyectouno/administrativo/forms.py
+++ ejemplo7/proyectouno/administrativo/forms.py
@@ -5,10 +5,18 @@ from administrativo.models import Matricula, Modulo, Estudiante
 
 
 class MatriculaForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(MatriculaForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Matricula
-        fields = ['estudiante', 'modulo', 'comentario','costo']
-
+        fields = ['estudiante', 'modulo', 'comentario', 'costo']
+        widgets = {
+            'comentario': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+        }
 
 
 class MatriculaEditForm(ModelForm):
@@ -19,23 +27,39 @@ class MatriculaEditForm(ModelForm):
         self.fields["estudiante"].widget = forms.widgets.HiddenInput()
         self.initial['modulo'] = self.instance.modulo
         self.fields["modulo"].widget = forms.widgets.HiddenInput()
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = Matricula
-        fields = ['estudiante', 'modulo', 'comentario','costo']
+        fields = ['estudiante', 'modulo', 'comentario', 'costo']
         widgets = {
             'comentario': forms.Textarea(attrs={
                 'rows': 4,
                 'cols': 40,
-                'placeholder': 'Escribe aquí tu comentario...'
-            }),}
+                'placeholder': 'Escribe aquí tu comentario...',
+                'class': 'form-control'
+            }), }
+
 
 class ModuloForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ModuloForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Modulo
         fields = ['nombre']
 
+
 class EstudianteForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(EstudianteForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Estudiante
-        fields = ['nombre', 'nombre','apellido','cedula','edad','tipo_estudiante']
+        fields = ['nombre', 'nombre', 'apellido', 'cedula', 'edad', 'tipo_estudiante']
